fb5dfe750f1c59818b6a4db4dc8f92cb1f4dfc35
HGShelton <hannahgradyshelton@gmail.com>
2024-05-28 21:24:16 -0400

    Added table for queries 3 & 4.

--- EmployeeSQL/data_analysis.sql
+++ EmployeeSQL/data_analysis.sql
@@ -16,3 +16,33 @@ SELECT first_name,
 	hire_date
 FROM employees
 WHERE EXTRACT(YEAR FROM hire_date) = 1986;
+
+
+--3. List the manager of each department along with their department number, 
+--department name, employee number, last name, and first name.
+SELECT dept_manager.dept_no,
+	departments.dept_name,
+	employees.emp_no,
+	employees.first_name,
+	employees.last_name
+FROM dept_manager
+INNER JOIN departments ON
+dept_manager.dept_no = departments.dept_no
+INNER JOIN employees ON
+dept_manager.emp_no = employees.emp_no;
+
+
+--4. List the department number for each employee along with that employee’s 
+--employee number, last name, first name, and department name.
+SELECT dept_emp.emp_no,
+	dept_emp.dept_no,
+	departments.dept_name,
+	employees.first_name,
+	employees.last_name
+FROM dept_emp
+INNER JOIN departments ON
+dept_emp.dept_no = departments.dept_no
+INNER JOIN employees ON
+dept_emp.emp_no = employees.emp_no;
+	
+
